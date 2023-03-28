3001129e9d25ed99a9c065c98e1a15f29b3386cc
dev-dong-su <fkdlrj1230@gmail.com>
2023-03-28 16:46:25 +0900

    feat: 이름 패턴 추가, mocking db설정

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './share/service/in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +18,15 @@ import { MainComponent } from './components/main/main.component';
     AppleDropComponent,
     MainComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
