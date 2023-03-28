ecef07e56eb5ae2c89ac32b309251adf8514b753
dev-dong-su <fkdlrj1230@gmail.com>
2023-03-28 16:17:25 +0900

    feat: create game, user, http service

---
+++ front/src/app/share/service/http.service.ts
@@ -0,0 +1,9 @@
+import { Injectable, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpService {
+  constructor() {}
+}
