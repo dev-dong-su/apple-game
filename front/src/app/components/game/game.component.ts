30ce11f474c8e14c892cec81903519a61ceb77b0
dev-dong-su <fkdlrj1230@gmail.com>
2023-03-29 17:18:43 +0900

    refactor: component 라우팅 수정

--- front/src/app/components/game/game.component.ts
+++ front/src/app/components/game/game.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-
+import { UserService } from '@app/share/service/user.service';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
 })
-export class GameComponent {}
+export class GameComponent {
+  constructor(private userService: UserService) {}
+}
