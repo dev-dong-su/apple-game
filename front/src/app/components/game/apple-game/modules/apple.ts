09ba090dc6902f3eb249c3a8bf1c555527cca2bf
dev-dong-su <fkdlrj1230@gmail.com>
2023-03-31 12:04:03 +0900

    feat: apple 반응형 격자 모양 렌더링, 랜덤 숫자

--- front/src/app/components/game/apple-game/modules/apple.ts
+++ front/src/app/components/game/apple-game/modules/apple.ts
@@ -1,7 +1,18 @@
 export class Apple {
-  private position: object;
+  public position: { x: number; y: number };
+  public image: HTMLImageElement;
+  public number: number;
+  public radius: number;
+  public gravity: number;
+  public mass: number;
 
-  constructor(x: number, y: number) {
-    this.position = { x: x, y: y };
+  constructor(x: number, y: number, radius: number) {
+    this.position = { x, y };
+    this.number = Math.floor(Math.random() * 10) + 1;
+    this.image = new Image();
+    this.image.src = 'assets/images/apple.png';
+    this.radius = radius;
+    this.mass = 1;
+    this.gravity = 0.1;
   }
 }
--- front/src/app/components/game/apple-game/modules/draw-canvas.ts
+++ front/src/app/components/game/apple-game/modules/draw-canvas.ts
@@ -1,25 +1,33 @@
 import { Drag } from '@components/game/apple-game/modules/drag';
+import { Apple } from './apple';
 
 export class DrawCanvas {
   private canvas: HTMLCanvasElement;
   private ctx: CanvasRenderingContext2D;
   private lastTime: number;
   private drag: Drag;
+  private units: Apple[];
+  private rect: any;
 
   constructor(canvas: HTMLCanvasElement) {
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d')!;
     this.ctx.strokeStyle = 'black';
     this.ctx.lineWidth = 2;
+    this.units = [];
     this.lastTime = new Date().getTime();
     this.drag = new Drag(canvas);
+    this.rect = this.canvas.parentElement!.getBoundingClientRect();
+    this.generateApples();
+
+    console.log(this.units);
+    console.log(this.rect);
   }
 
   init(): void {
     window.onresize = () => {
-      const rect = this.canvas.parentElement!.getBoundingClientRect();
-      const width = rect.width;
-      const height = rect.height;
+      const width = this.rect.width;
+      const height = this.rect.height;
       const devicePixelRatio = window.devicePixelRatio || 1;
 
       this.canvas.width = width * devicePixelRatio;
@@ -29,6 +37,7 @@ export class DrawCanvas {
       this.canvas.style.height = `${height}px`;
 
       this.ctx.scale(devicePixelRatio, devicePixelRatio);
+      console.log((width * devicePixelRatio) / 15);
     };
 
     window.onresize(new UIEvent('resize'));
@@ -38,6 +47,25 @@ export class DrawCanvas {
     this.canvas.onmouseup = () => this.drag.onMouseUp();
 
     this.canvas.onmousemove = (event) => this.drag.onMouseMove(event);
+  }
+
+  private generateApples(): void {
+    const rows = 10;
+    const columns = 17;
+
+    const availableWidth = this.rect.width / columns;
+    const availableHeight = this.rect.height / rows;
+
+    const appleRadius = Math.min(availableWidth, availableHeight) * 0.4;
+
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
+        const x = j * availableWidth + availableWidth / 2 - appleRadius;
+        const y = i * availableHeight + availableHeight / 2 - appleRadius;
+        const apple = new Apple(x, y, appleRadius);
+        this.units.push(apple);
+      }
+    }
   }
 
   update(): void {
@@ -55,12 +83,33 @@ export class DrawCanvas {
       this.ctx.stroke();
     }
 
+    this.units.forEach((apple) => {
+      this.ctx.drawImage(
+        apple.image,
+        apple.position.x,
+        apple.position.y,
+        apple.radius * 2,
+        apple.radius * 2
+      );
+
+      this.ctx.font = `${apple.radius}px Poor Story`;
+      this.ctx.fillStyle = '#FFFFFF';
+      this.ctx.textAlign = 'center';
+      this.ctx.textBaseline = 'middle';
+      this.ctx.fillText(
+        apple.number.toString(),
+        apple.position.x + apple.radius,
+        apple.position.y + apple.radius
+      );
+    });
+
+    this.ctx.font = `12px Poor Story`;
     this.ctx.fillStyle = '#000000';
     this.ctx.fillText(
       parseInt(String(1000 / (new Date().getTime() - Number(this.lastTime)))) +
         ' fps',
-      this.canvas.width / window.devicePixelRatio - 50,
-      20
+      this.canvas.width / window.devicePixelRatio - 20,
+      10
     );
 
     this.lastTime = new Date().getTime();
