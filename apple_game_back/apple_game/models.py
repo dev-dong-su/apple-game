c48ba5f772d4b35b3dd2665308c5acf760746231
dev-dong-su <fkdlrj1230@gmail.com>
2023-04-04 10:38:37 +0900

    feat: refactor 폴더 이름 변경, secret_key 분리

---
+++ apple_game_back/apple_game/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=255)
+    best_score = models.IntegerField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.username
+
+class Score(models.Model):
+    score = models.IntegerField()
+    user = models.ForeignKey(User, related_name='scores', on_delete=models.CASCADE)
+    user_best_score = models.ForeignKey(User, related_name='best_scores', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username}: {self.score}"
