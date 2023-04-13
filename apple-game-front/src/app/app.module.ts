faa1a4304e4d135280f66635c1203c133d567914
dev-dong-su <fkdlrj1230@gmail.com>
2023-04-13 16:37:11 +0900

    fix: update전 커밋

--- apple-game-front/src/app/app.module.ts
+++ apple-game-front/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -13,6 +13,7 @@ import { MainComponent } from './components/main/main.component';
 import { AppleGameComponent } from './components/game/apple-game/apple-game.component';
 import { RankingComponent } from './components/ranking/ranking.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,12 @@ import { MenuComponent } from './components/menu/menu.component';
     FormsModule,
     HttpClientModule,
     JwtModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [JwtHelperService],
   bootstrap: [AppComponent],
